f4759e6b48513fe396462bfe119e177046660ea4
work <danmmwx@gmail.com>
2024-10-29 03:24:30 +0300

    Display articles cards

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import { ArticleCardData } from "@/lib/models";
+
+type Props = {
+  cardData: ArticleCardData;
+};
+
+export const ArticleCard = ({ cardData }: Props) => {
+  const href = `/article/${cardData.slug}`;
+
+  return (
+    <a href={href}>
+      <div
+        className="max-w-[376px] h-full bg-[#1B2022] mx-0 sm:mx-medium p-4 my-medium shadow-[0px_8px_16px_0px_rgba(0,0,0,0.15)] rounded-lg flex flex-col items-start justify-between"
+      >
+        <div>
+          <Image
+            src={cardData?.cover as string}
+            alt={cardData.title}
+            className="w-full h-[130px]"
+            width={100}
+            height={100}
+            placeholder="blur"
+            blurDataURL={cardData?.cover}
+            unoptimized
+          />
+          <div className="mt-4 text-xl sm:text-2xl text-white text-start font-bold leading-normal line-clamp-3">
+            {cardData.title}
+          </div>
+          <div className="mt-3 text-[15px] text-white/50 text-start font-normal leading-normal line-clamp-4">
+            {cardData.summary}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
